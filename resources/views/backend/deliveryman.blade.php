feaacc08bef2097a4f607cf9319c38a06f925638
sharif0606 <shariful0606@gmail.com>
2024-03-05 18:06:20 +0600

    change blade page

--- resources/views/backend/deliveryman.blade.php
+++ resources/views/backend/deliveryman.blade.php
@@ -13,69 +13,6 @@
 <main class="content px-3 py-4">
     <div class="container-fluid">
         <div class="mb-3">
-            <h3 class="fw-bold fs-4 mb-3">Admin Dashboard</h3>
-            <div class="row">
-                <div class="col-12 col-md-4 ">
-                    <div class="card border-0">
-                        <div class="card-body py-4">
-                            <h5 class="mb-2 fw-bold">
-                                Memebers Progress
-                            </h5>
-                            <p class="mb-2 fw-bold">
-                                $72,540
-                            </p>
-                            <div class="mb-0">
-                                <span class="badge text-success me-2">
-                                    +9.0%
-                                </span>
-                                <span class=" fw-bold">
-                                    Since Last Month
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-4 ">
-                    <div class="card  border-0">
-                        <div class="card-body py-4">
-                            <h5 class="mb-2 fw-bold">
-                                Memebers Progress
-                            </h5>
-                            <p class="mb-2 fw-bold">
-                                $72,540
-                            </p>
-                            <div class="mb-0">
-                                <span class="badge text-success me-2">
-                                    +9.0%
-                                </span>
-                                <span class="fw-bold">
-                                    Since Last Month
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-4 ">
-                    <div class="card border-0">
-                        <div class="card-body py-4">
-                            <h5 class="mb-2 fw-bold">
-                                Memebers Progress
-                            </h5>
-                            <p class="mb-2 fw-bold">
-                                $72,540
-                            </p>
-                            <div class="mb-0">
-                                <span class="badge text-success me-2">
-                                    +9.0%
-                                </span>
-                                <span class="fw-bold">
-                                    Since Last Month
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <h3 class="fw-bold fs-4 my-3">Avg. Agent Earnings
             </h3>
             <div class="row">
@@ -105,3 +42,4 @@
         </div>
     </div>
 </main>
+@section
