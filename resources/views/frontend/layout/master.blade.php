1cde4521a64c7e06f667344868a8e678cca216cc
Isfar Nayen <moriyaty133@gmail.com>
2024-03-09 20:46:09 +0600

    no change

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -16,12 +16,14 @@
 
 <body>
     <div class="all-content">
+
         <nav class="navbar navbar-expand-lg navbar-light px-4 border-bottom">
             <div class="container-fluid">
                 <a class="navbar-brand fs-2" href="#">Delivery<span class="text-primary">Swift</span></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
+
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
