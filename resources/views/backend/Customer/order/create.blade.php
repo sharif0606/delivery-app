6dd8b87e5daa3912f2aa8cafe0d85a39bbb62c8f
afifakhanam01 <u1908001@student.cuet.ac.bd>
2024-03-10 17:26:34 +0600

    create

---
+++ resources/views/backend/Customer/order/create.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Form</title>
+    <link rel="stylesheet" href="order_form.css">
+</head>
+<body>
+    <h1>Order Form</h1>
+    
+
+    <script src="order_form.js"></script>
+</body>
+</html>
