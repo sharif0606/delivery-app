240b59c5a921c61759d6c56be5c0afbcf0f8d684
Isfar Nayen <moriyaty133@gmail.com>
2024-03-28 15:06:57 +0600

    incoming errors

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,19 +10,10 @@ class HomeController extends Controller
     public function index(){
         return view('frontend.home');
     }
-
     public function orderTrack(){
         return view('frontend.ordertrack');
     }
-<<<<<<< HEAD
     public function service(){
         return view('frontend.service');
     }
-
-=======
-
-    public function service(){
-        return view('frontend.service');
-    }
->>>>>>> d6b2647d14408694c48781d72f64f47fb103285d
 }
--- resources/views/frontend/service.blade.php
+++ resources/views/frontend/service.blade.php
@@ -1,7 +1,6 @@
 @extends('frontend.layout.master')
 
 @section('content')
-<<<<<<< HEAD
     <div class="container"> 
     <div class="service-wrapper">
         <div class="service">
@@ -38,44 +37,3 @@
     </div>
     
     @endsection
-=======
-
-<div class="container"> 
-<div class="service-wrapper">
-    <div class="service">
-        <h1>Our Services</h1>
-        <p>Trusted Partner for Seamless Deliveries</p>
-        <div class="cards"> 
-            <div class="card">
-                <i class="fas fa-truck"></i>
-                <h2>Standard Delivery</h2>
-                <p>Count on our dependable service with clear delivery timelines tailored to your needs.</p>
-        
-            </div>
-            <div class="card">
-                <i class="fas fa-map"></i>
-                <h2>Coverage Area</h2>
-                <p>Enjoy nationwide reach, ensuring your packages reach every corner of Bangladesh seamlessly.</p>
-            </div>
-            <div class="card">
-                <i class="fas fa-money-bill"></i>
-                <h2>Shipping Rates and Pricing</h2>
-                <p>Explore our transparent pricing structure, providing clarity on costs based on package specifications and delivery preferences.</p>
-        
-            </div>
-            <div class="card">
-                <i class="fas fa-headset"></i>
-                <h2>Customer Support</h2>
-                <p>Experience peace of mind with our dedicated support team, available through various channels to assist you with any concerns you may have.  </p>
-        
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-@endsection
-
-@push('scripts')
-<script src="{{asset('frontend/js/script.js')}}"></script>
-@endpush
->>>>>>> d6b2647d14408694c48781d72f64f47fb103285d
