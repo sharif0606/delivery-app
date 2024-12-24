2f608d709bcf6ae64dcb68840235faaff2d31570
Isfar Nayen <moriyaty133@gmail.com>
2024-12-24 12:19:36 +0600

    changes

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -16,8 +16,5 @@ class HomeController extends Controller
     public function service(){
         return view('frontend.service');
     }
-    public function service(){
-        return view('frontend.service');
-    }
 
 }
